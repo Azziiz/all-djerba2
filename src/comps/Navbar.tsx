8fa795ae12d005d8a36c79796e343c9dcdb9adae
aziz_belhaj <112778142+Azziiz@users.noreply.github.com>
2023-03-28 00:39:24 +0100

    Update Navbar.tsx

--- src/comps/Navbar.tsx
+++ src/comps/Navbar.tsx
@@ -8,13 +8,13 @@ export default function Navbar() {
    
         <header className="navbar">
             <div className="logo">
-                <h1 id="title"><a href='#1'>Djerba <span>Wonders</span></a></h1>
+                <h1 id="title"><a href='/#1'>Djerba <span>Wonders</span></a></h1>
             </div>
                 <nav>
                     <ul>
-                        <li><a href="#2" id="beach"><span className="material-symbols-outlined">beach_access</span></a></li>
-                        <li><a href="#3" id="history"><span className="material-symbols-outlined">museum</span></a></li>
-                        <li><a href="#4" id="culture"><span className="material-symbols-outlined">draw</span></a></li>
+                        <li><a href="/#2" id="beach"><span className="material-symbols-outlined">beach_access</span></a></li>
+                        <li><a href="/#3" id="history"><span className="material-symbols-outlined">museum</span></a></li>
+                        <li><a href="/#4" id="culture"><span className="material-symbols-outlined">draw</span></a></li>
                     </ul>
                 </nav>
                 
